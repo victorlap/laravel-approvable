b68108ef453e1039b48348ec9b067ba09c03f623
Victor Lap <victorlap@outlook.com>
2017-06-28 15:02:45 +0200

    Add some tests

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,6 +2,7 @@
 
 namespace Victorlap\Approvable\Tests;
 
+use Illuminate\Support\Facades\DB;
 use Victorlap\Approvable\Tests\Models\UserCanApprove;
 use Victorlap\Approvable\Tests\Models\UserCannotApprove;
 
@@ -14,7 +15,26 @@ class BaseTests extends TestCase
     }
 
     public function testRegularCanCreate() {
-        $user = UserCannotApprove::create(['name' => 'John Doe', 'email' => 'john2@doe.com']);
+        $user = UserCannotApprove::create(['name' => 'Jane Doe', 'email' => 'jane@doe.com']);
         $this->assertTrue($user->exists);
+    }
+
+    public function testApproverCanEdit() {
+        $user = UserCanApprove::find(['email' => 'john@doe.com']);
+
+        $user->update(['name' => 'Doe John'])->fresh();
+
+        $this->assertEquals('Doe Jonh', $user->name);
+    }
+
+    public function testRegularCannotEdit() {
+        $user = UserCannotApprove::find(['email' => 'jane@doe.com']);
+
+        $this->assertEquals(0, DB::table('approvals')->count());
+
+        $user->update(['name' => 'Doe Jane'])->fresh();
+
+        $this->assertEquals('Jane Doe', $user->name);
+        $this->assertEquals(1, DB::table('approvals')->count());
     }
 }
